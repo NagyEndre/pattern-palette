b1f38b1c4fb4379d17655be6fca28a44d8843567
Endre Nagy <endretamasnagy@gmail.com>
2023-04-09 21:26:18 +0200

    FEAT: Add TemplateMethod exercise

---
+++ src/exercises/TemplateMethod.ts
@@ -0,0 +1,23 @@
+abstract class TestSequence {
+  public execute() {
+    this.prepareEnvironment();
+    this.userPreparation();
+    this.run();
+    this.userCleanUp();
+    this.collectResults();
+  }
+
+  protected abstract prepareEnvironment(): void;
+
+  protected userPreparation(): void {
+    console.log("No user preparation is executed.");
+  }
+
+  protected abstract run(): void;
+
+  protected userCleanUp(): void {
+    console.log("No user clean up is executed.");
+  }
+
+  protected abstract collectResults(): void;
+}
